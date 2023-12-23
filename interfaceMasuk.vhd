1b71621c3f5b8efe61878c524a092727a1428c8e
kamalMakarim <kamal.makarim@gmail.com>
2023-12-23 21:59:30 +0700

    contoh get timestamp

--- interfaceMasuk.vhd
+++ interfaceMasuk.vhd
@@ -4,7 +4,7 @@ use IEEE.numeric_std.all;
 
 entity interfaceMasuk is
     port (
-        INSTRUCTION : OUT STD_LOGIC_VECTOR (8 DOWNTO 0);
+        INSTRUCTION : OUT STD_LOGIC_VECTOR (10 DOWNTO 0);
         DATA_IN : OUT STD_LOGIC_VECTOR (63 DOWNTO 0);
         DONE : IN STD_LOGIC;
         WANT_TO_USE : OUT STD_LOGIC;
@@ -16,52 +16,58 @@ entity interfaceMasuk is
 end entity interfaceMasuk;
 
 architecture rtl of interfaceMasuk is
-    type state_type is (STAND_BY, GET_TIMESTAMP, CARDID_TIMESTAMP);
-    signal current_state : state_type := IDLE;
-
+    type state_type is (STAND_BY, GET_TIMESTAMP, CARDID_TIMESTAMP, MOV_TO_REG, MOV_TO_MEM, SAVE);
+    signal next_state : state_type := IDLE;
+    
 begin
     process(CLK)
     begin
         if rising_edge(CLK) then
-            case current_state is
+            case next_state is
                 when STAND_BY =>
+                    WANT_TO_USE <= '0';
                     if  CARD_ID /= (others => '0') then
+                        WANT_TO_USE <= '1';
                         if CPU_NOT_READY = '0' then
-                            current_state <= GET_TIMESTAMP;
+                            next_state <= GET_TIMESTAMP;
                         end if; 
                     else
-                        current_state <= STAND_BY;
+                        next_state <= STAND_BY;
                     end if;
                 when GET_TIMESTAMP =>
+                    INSTRUCTION <= "01100000000";
                     if DONE = '1' then
-                        current_state <= CARDID_TIMESTAMP;
+                        next_state <= CARDID_TIMESTAMP;
                     else
-                        current_state <= GET_TIMESTAMP;
+                        next_state <= GET_TIMESTAMP;
                     end if;
                 when CARDID_TIMESTAMP =>
                     if DONE = '1' then
-                        current_state <= STAND_BY;
+                        next_state <= STAND_BY;
                     else
-                        current_state <= CARDID_TIMESTAMP;
+                        next_state <= CARDID_TIMESTAMP;
+                    end if;
+                when MOV_TO_REG =>
+                    if DONE = '1' then
+                        next_state <= STAND_BY;
+                    else
+                        next_state <= MOV_TO_REG;
+                    end if;
+                when MOV_TO_MEM =>
+                    if DONE = '1' then
+                        next_state <= STAND_BY;
+                    else
+                        next_state <= MOV_TO_MEM;
+                    end if;
+                when SAVE =>
+                    if DONE = '1' then
+                        next_state <= STAND_BY;
+                    else
+                        next_state <= SAVE;
                     end if;
                 when others =>
-                    current_state <= STAND_BY;
+                    next_state <= STAND_BY;
             end case;
-        end if;
-    end process;
-end architecture rtl;
-    
-    
-begin
-    process(CLK)
-    begin
-        if rising_edge(CLK) then
-            if WANT_TO_USE = '1' then
-                CPU_NOT_READY <= '1';
-                DATA_OUT <= DATA_IN;
-            else
-                CPU_NOT_READY <= '0';
-            end if;
         end if;
     end process;
 end architecture rtl;
