fa7280ff1830a3570ca5b1d49db13383ee3c64cc
kamalMakarim <kamal.makarim@gmail.com>
2023-12-24 21:32:04 +0700

    no compile error

--- parking_memory.vhd
+++ parking_memory.vhd
@@ -10,18 +10,20 @@ entity parking_memory is
         data_in : in  std_logic_vector(63 downto 0); -- 64 bits for combined data
         wr_en   : in  std_logic;
         addr    : in  std_logic_vector(7 downto 0);  -- 8-bit address
-        data_out: out std_logic_vector(63 downto 0)
+        data_out: out std_logic_vector(63 downto 0);
+        shift   : in  std_logic
     );
 end entity parking_memory;
 
 architecture behavioral of parking_memory is
 
     type memory_type is array (0 to 255) of std_logic_vector(63 downto 0); -- 256 entries
-    signal memory_array : memory_type;
+    signal memory_array : memory_type := (others => (others => '0')); -- Initialize to zero
     signal addr_temp : integer;
 
     begin
     process(clk, rst)
+        variable addr_int : integer;
         begin
             if rst = '1' then
                 memory_array <= (others => (others => '0'));  -- Initialize to zero
@@ -30,6 +32,13 @@ architecture behavioral of parking_memory is
                     addr_temp <= to_integer(unsigned(addr));
                     memory_array(addr_temp) <= data_in;
                 end if;
+            end if;
+            if shift = '1' then
+                addr_int := to_integer(unsigned(addr));
+                for i in addr_int to 254 loop
+                    memory_array(i) <= memory_array(i+1);
+                end loop;
+                memory_array(255) <= (others => '0');
             end if;
     end process;
 
